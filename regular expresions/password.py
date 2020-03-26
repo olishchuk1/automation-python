ff30f593661af679eead2f20183f4b4c297269db
olishchuk1 <30534182+olishchuk1@users.noreply.github.com>
2020-03-26 18:51:01 +0200

    added comments

--- regular expresions/password.py
+++ regular expresions/password.py
@@ -1,17 +1,19 @@
 import re
+# creating some rules for password
 password_regex = (
-    re.compile(r'\d'),
-    re.compile(r'[a-z]'),
-    re.compile(r'[A-Z]'),
-    re.compile(r'.{6,}'),
-    re.compile(r'!|@|#|$|%|^|&|\*|(|)|_|\+|=|-'),
-    re.compile(r'.{12,}')
+    re.compile(r'\d'), # a digit
+    re.compile(r'[a-z]'), # a lowercase letter
+    re.compile(r'[A-Z]'), # an uppercase letter
+    re.compile(r'.{6,}'), # length six and more
+    re.compile(r'!|@|#|$|%|^|&|\*|(|)|_|\+|=|-'), # a special symbol
+    re.compile(r'.{12,}')  # length 12 and more
     )
 
 password  = input()
 
 counter = 0
 for rx in password_regex:
+    # counts how many rules are performed
     if rx.search(password):
         counter += 1
 
